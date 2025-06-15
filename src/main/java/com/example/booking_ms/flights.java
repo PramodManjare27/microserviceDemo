b40bbbd4ffb089f714bac9f254e77f4118038724
PramodManjare27 <pramod.manjare27@gmail.com>
2025-06-15 10:20:12 +0530

    initial commit

---
+++ src/main/java/com/example/booking_ms/flights.java
@@ -0,0 +1,13 @@
+package com.example.booking_ms;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class flights {
+    @GetMapping("/flight")
+    public String getData() {
+        return "Please return flight ticket with 30% discount"
+    }
+
+}
